bbc4d4647fe90956af228fb7d574d54276285300
guilhermezanpaiva-afke <guilherme.zan.paiva@gmail.com>
2026-09-01 00:47:05 -0300

    Create remocao_colunas.sql

---
+++ sql/limpeza-analise/remocao_colunas.sql
@@ -0,0 +1,25 @@
+-- ============================================================
+-- IMUNE - Health Investment Advisor
+-- Remoção de Colunas
+-- ============================================================
+
+ALTER TABLE BARI
+    
+DROP (
+    AP_MVM,
+    AP_CONDIC,
+    AP_GESTAO,
+    AP_CODUNI,
+    AP_AUTORIZ,
+    AP_CMP,
+    AP_PRIPAL,
+    AP_VL_AP,
+    AP_UFMUN,
+    AP_TPUPS,
+    AP_TIPPRE,
+    AP_MN_IND,
+    AP_CNSPCN,
+    AP_COIDADE,
+    AP_MUNPCN
+);
+
